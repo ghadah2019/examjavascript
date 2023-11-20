2cb0b81913945497a4755fc4d815e8ceef162b0d
ghadah2019 <ghada__o@hotmail.com>
2023-11-20 14:59:22 +0300

    new

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ fetch("http://api.aladhan.com/v1/calendar/2017/4?latitude=51.508515&longitude=-0
 
         // let time = info.data[0].timings;
         // console.log(time);
+
+
+        let m1 = info.data[0].date.gregorian.date
+        console.log(m1);
+
+        let hijri1 = info.data[0].date.readable
+        
+
+
       
 
         let hijri = info.data.map(a => a.timings);
